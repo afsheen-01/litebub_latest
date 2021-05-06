762b81033b2e28547edb0547d049744f59042ceb
Afsheen Amroliwala <afsheenamroliwala@gmail.com>
2021-05-07 01:19:26 +0530

    remove showguide

--- src/CreateLink.js
+++ src/CreateLink.js
@@ -126,7 +126,7 @@ const Address = ({ setForm, formData, navigation }) => {
     yRadius: 140,
     xRadius: 250,
     cornerRadius: 40,
-    showGuides: true,
+    showGuides: false,
     compact: false,
     gravitation: 5
   }
