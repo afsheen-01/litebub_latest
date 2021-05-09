58a5c0ac12279f6e2a4731e4873eea0daa925c9d
Afsheen Amroliwala <afsheenamroliwala@gmail.com>
2021-05-09 10:58:46 +0530

    missions 0506

--- src/CreateLink.js
+++ src/CreateLink.js
@@ -28,6 +28,8 @@ const Address = ({ setForm, formData, navigation }) => {
           setTopics(data)
         }
       })
+    const topicInput = document.querySelector('.topic-input')
+    topicInput.focus()
   }, [])
   function createRoom() {
     // console.log(formData);
@@ -114,17 +116,17 @@ const Address = ({ setForm, formData, navigation }) => {
         height: obj.size == "big" ? `${100}%` : obj.size == "medium" ? `${80}%` : `${65}%`,
         width: obj.size == "big" ? `${100}%` : obj.size == "medium" ? `${80}%` : `${65}%`,
         fontSize: obj.size == "big" ? "1em" : obj.size == "medium" ? "0.9em" : "0.85em",
-      }} key={i} id={i}>{obj.topic}</div>)
+      }} key={i} id={i}>{obj.topic.replace(" ","\n")}</div>)
   })
   const options = {
     size: 140,
     minSize: 50,
     provideProps: true,
-    numCols: 5,
-    gutter: 0,
-    fringeWidth: 10,
-    yRadius: 140,
-    xRadius: 250,
+    numCols: 6,
+    gutter: 5,
+    fringeWidth: 0,
+    yRadius: 50,
+    xRadius: 400,
     cornerRadius: 40,
     showGuides: false,
     compact: false,
@@ -180,17 +182,17 @@ const Address = ({ setForm, formData, navigation }) => {
       </div>
       <div 
       className="form" 
-      style = {{height: "100%",
+      style = {{height: "100%",  
       // border:"1px solid #000"
     }}
       >
         <div className = "center-it-cl">
-        <div className="chatAbout">what's the chat about?
-        </div>
-        <div
-          className = "cl-inputSec"
-        >
-        <input
+          <div className="chatAbout">what's the chat about?
+          </div>
+          <div
+            className = "cl-inputSec"
+          >
+            <input
               className = "ui input massive topic-input"
               name="topic"
               //value={topic}
@@ -206,28 +208,28 @@ const Address = ({ setForm, formData, navigation }) => {
                 margin: "14px 0 0 ",
                 paddingTop: 0,
                 paddingRight:0
-            }}       
-              />
-              <svg
-                onClick = {emptyInp}
-                style = {{
+              }}       
+            />
+            <svg
+              onClick = {emptyInp}
+              style = {{
                   visibility: "hidden"
                 }}
-                className  = "cl-crossBtn"
-               width="50" height="65" viewBox="0 0 55 73" fill="none" xmlns="http://www.w3.org/2000/svg">
+              className  = "cl-crossBtn"
+              width="50" height="65" viewBox="0 0 55 73" fill="none" xmlns="http://www.w3.org/2000/svg">
               <g opacity="0.42">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M45.1546 19.9591C43.8144 18.6188 41.6412 18.619 40.3007 19.9595L28.9787 31.2815L17.8737 20.1765C16.5335 18.8362 14.3603 18.8364 13.0198 20.1769C11.6793 21.5174 11.6791 23.6906 13.0194 25.0309L24.1243 36.1358L12.8042 47.456C11.4637 48.7965 11.4635 50.9697 12.8038 52.3099C14.1441 53.6502 16.3173 53.65 17.6578 52.3095L28.9779 40.9894L40.0817 52.0933C41.422 53.4336 43.5952 53.4334 44.9357 52.0929C46.2762 50.7524 46.2764 48.5792 44.9361 47.2389L33.8322 36.135L45.1542 24.813C46.4947 23.4725 46.4949 21.2993 45.1546 19.9591Z" fill="white"/>
               </g>
-              </svg>
-              </div>
+            </svg>
+          </div>
           <button  className = "ui button circular huge cbtn" id = "create-link-btn"
-          disabled = {btnState}
-          style = {{
-            background: 'transparent',
-            border: "2px solid #fff",
-            marginTop: 18
-          }}
-          onClick={createRoom}>
+            disabled = {btnState}
+            style = {{
+              background: 'transparent',
+              border: "2px solid #fff",
+              marginTop: 18
+            }}
+            onClick={createRoom}>
             {/* <Icon> */}
             <svg width="30" height="21" viewBox="3.5 0 30 26.89" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M0.167294 25.1068L1.7813 26.7208C1.8926 26.8321 2.0387 26.888 2.18481 26.888C2.33091 26.888 2.47702 26.8321 2.58831 26.7208L7.83327 21.4764C7.83384 21.4758 7.83499 21.4752 7.83556 21.4747C7.83613 21.4741 7.83613 21.473 7.83727 21.4718L18.7313 10.5784C18.8386 10.4711 18.8985 10.3261 18.8985 10.1749C18.8985 10.0236 18.8386 9.87811 18.7313 9.77139L17.1161 8.15681C16.893 7.93365 16.5323 7.93365 16.3091 8.15681L5.4134 19.0531C5.4134 19.0537 5.4134 19.0537 5.4134 19.0537C5.41283 19.0537 5.41283 19.0542 5.41283 19.0542L4.20346 20.263C4.20346 20.2636 4.20346 20.2636 4.20346 20.2636C4.20289 20.2636 4.20289 20.2642 4.20289 20.2642L0.167864 24.2992C-0.0558598 24.5223 -0.0558595 24.8836 0.167294 25.1068ZM16.7132 9.36788L17.5208 10.1755L7.43148 20.2642L6.62334 19.4572L16.7132 9.36788ZM5.81633 20.2647L6.62391 21.0712L6.21983 21.4752L5.41283 20.6677L5.81633 20.2647ZM4.60582 21.4752L5.41283 22.2823L2.18481 25.5103L1.3778 24.7033L4.60582 21.4752Z" fill="white"/>
@@ -237,19 +239,17 @@ const Address = ({ setForm, formData, navigation }) => {
               <path d="M22.8294 12.5559C23.7733 12.5559 24.5415 11.7878 24.5415 10.8438C24.5415 9.8998 23.7733 9.1316 22.8294 9.1316C21.8854 9.1316 21.1172 9.8998 21.1172 10.8438C21.1172 11.7878 21.8854 12.5559 22.8294 12.5559ZM22.8294 10.273C23.1438 10.273 23.4001 10.5293 23.4001 10.8438C23.4001 11.1582 23.1438 11.4145 22.8294 11.4145C22.5149 11.4145 22.2586 11.1582 22.2586 10.8438C22.2586 10.5293 22.5149 10.273 22.8294 10.273Z" fill="white"/>
               <path d="M5.13674 15.4096C6.08072 15.4096 6.84891 14.6414 6.84891 13.6974C6.84891 12.7534 6.08072 11.9852 5.13674 11.9852C4.19276 11.9852 3.42456 12.7534 3.42456 13.6974C3.42456 14.6414 4.19276 15.4096 5.13674 15.4096ZM5.13674 13.1267C5.45121 13.1267 5.70746 13.3829 5.70746 13.6974C5.70746 14.0124 5.45121 14.2681 5.13674 14.2681C4.82227 14.2681 4.56601 14.0124 4.56601 13.6974C4.56601 13.3824 4.82227 13.1267 5.13674 13.1267Z" fill="white"/>
               <path d="M14.8391 2.2829C14.8391 1.33892 14.0709 0.570724 13.127 0.570724C12.183 0.570724 11.4148 1.33892 11.4148 2.2829C11.4148 3.22688 12.183 3.99507 13.127 3.99507C14.0709 3.99507 14.8391 3.22688 14.8391 2.2829ZM12.5562 2.2829C12.5562 1.96843 12.8125 1.71217 13.127 1.71217C13.4414 1.71217 13.6977 1.96843 13.6977 2.2829C13.6977 2.59737 13.4414 2.85362 13.127 2.85362C12.8125 2.85362 12.5562 2.59737 12.5562 2.2829Z" fill="white"/>
-          </svg>
-         <span style = {{paddingBottom:"4em",
-height: "inherit",
-// border: "2px solid #fff"
-}}
-  >Create Link</span>
+            </svg>
+            <span style = {{paddingBottom:"4em",
+              height: "inherit",
+              // border: "2px solid #fff"
+              }}
+            >Create Link</span>
           </button>
         </div>
-        {/* <div className = "bubblesDiv"> */}
         <BubbleUI options = {options} className = "bubbleUI">
           {bubbles}
         </BubbleUI>
-        {/* </div> */}
       </div>
     </div>
   );
