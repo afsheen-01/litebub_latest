f8ec617996ab523c42e88911205ee61382fd6052
afsheen-01 <58872364+afsheen-01@users.noreply.github.com>
2021-06-18 09:34:48 +0530

    user cant thwack themselves - remaining

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -415,15 +415,15 @@ export default function ChatRoom() {
               <>
                 {notifMsg.text}
                   <svg
-                    width="25"
-                    height="27"
-                    viewBox="0 0 25 27"
+                    width="23"
+                    height="15"
+                    viewBox="0 0 15 23"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
                       d="M16.8839 21.8431L12.573 16.7947L6.81582 21.398L10.4378 14.223L4.05593 8.17166L11.1602 11.4466L15.5149 2.45204L14.1079 12.2025L19.5464 9.7748L17.6357 14.0075L24.8635 19.4327L15.8225 16.0409L16.8839 21.8431Z"
-                      stroke="FF0000"
+                      stroke="#FF0000"
                       stroke-width="1.39649"
                     />
                   </svg>
