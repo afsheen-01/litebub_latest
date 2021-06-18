11478aae838e4d0522579c49b42a2f4e08dd5563
afsheen-01 <58872364+afsheen-01@users.noreply.github.com>
2021-06-18 09:37:02 +0530

    links updated

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -712,7 +712,7 @@ export default function ChatRoom() {
 
   function copyRoomID() {
     // console.log("https://nz45o.csb.app/room/" + id);
-    copyToClipboard("https://hqzon.csb.app/room/" + id);
+    copyToClipboard("https://wdf2h.csb.app/room/" + id);
     const linkCopy = document.querySelector(".linkCopied");
 		linkCopy.style.visibility = "visible";
 		let timerID = setTimeout(() => {
