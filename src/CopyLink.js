f406a115dc8af06bbd1efc62cf1e2f726bc22990
afsheen-01 <58872364+afsheen-01@users.noreply.github.com>
2021-07-05 14:27:11 +0530

    updated links

--- src/CopyLink.js
+++ src/CopyLink.js
@@ -97,7 +97,7 @@ const Contact = ({ formData, navigation }) => {
 	var roomID = formData.roomId;
 	function copyRoomID() {
 		// console.log("https://hkwft.csb.app/room/" + roomID);
-		copyToClipboard("https://dh19w.csb.app/room/" + roomID + `?avatarNum=${chatAvatar}&avatarColor=${chatColor}&chatBg=${chatBg}`);
+		copyToClipboard("https://ou5b7.csb.app/room/" + roomID + `?avatarNum=${chatAvatar}&avatarColor=${chatColor}&chatBg=${chatBg}`);
 		// console.log(chatBg)
 
 		html2canvas(document.getElementById("url-preview"), {
