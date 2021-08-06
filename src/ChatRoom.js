0a4154b6eadd4f1062d4db7b0b6fb3604ff869c7
afsheen-01 <58872364+afsheen-01@users.noreply.github.com>
2021-08-06 07:40:15 +0530

    cleanup

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1384,13 +1384,11 @@ export default function ChatRoom() {
                               <path
                                 d="M16.8839 21.8431L12.573 16.7947L6.81582 21.398L10.4378 14.223L4.05593 8.17166L11.1602 11.4466L15.5149 2.45204L14.1079 12.2025L19.5464 9.7748L17.6357 14.0075L24.8635 19.4327L15.8225 16.0409L16.8839 21.8431Z"
                                 stroke = {thwackColor(item, userName)}
-                                // stroke={item.thwacks > 0 ? "#FF2020" : "#DBDBDB"}
                                 stroke-width="1.39649"
                               />
                             </svg>
                           </div>
                         </div>: null}
-                        {/* {item.thwacks >= 3?thwackComponent():null} */}
                       
                       {/* end */}
                     </div>
@@ -1512,11 +1510,7 @@ export default function ChatRoom() {
                                     >
                                       <path
                                         d="M16.8839 21.8431L12.573 16.7947L6.81582 21.398L10.4378 14.223L4.05593 8.17166L11.1602 11.4466L15.5149 2.45204L14.1079 12.2025L19.5464 9.7748L17.6357 14.0075L24.8635 19.4327L15.8225 16.0409L16.8839 21.8431Z"
-                                        stroke={
-                                          itm.thwacks > 0
-                                            ? "#FF2020"
-                                            : "#DBDBDB"
-                                        }
+                                        stroke={thwackColor(itm, userName)}
                                         stroke-width="1.39649"
                                       />
                                     </svg>
@@ -1619,11 +1613,7 @@ export default function ChatRoom() {
                                     >
                                       <path
                                         d="M16.8839 21.8431L12.573 16.7947L6.81582 21.398L10.4378 14.223L4.05593 8.17166L11.1602 11.4466L15.5149 2.45204L14.1079 12.2025L19.5464 9.7748L17.6357 14.0075L24.8635 19.4327L15.8225 16.0409L16.8839 21.8431Z"
-                                        stroke={
-                                          item.thwacks > 0
-                                            ? "#FF2020"
-                                            : "#DBDBDB"
-                                        }
+                                        stroke={thwackColor(item, userName)}
                                         stroke-width="1.39649"
                                       />
                                     </svg>
