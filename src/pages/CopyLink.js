f224252cb2bdf0301915faed1f44164b136bd678
afsheen-01 <58872364+afsheen-01@users.noreply.github.com>
2021-11-04 11:06:36 +0530

    abrar's changes

--- src/pages/CopyLink.js
+++ src/pages/CopyLink.js
@@ -100,7 +100,7 @@ const Contact = ({ formData, navigation }) => {
 	function copyRoomID() {
 		// console.log("https://hkwft.csb.app/room/" + roomID);
 		copyToClipboard(
-			"https://4rzux.csb.app/room/" +
+			"https://nzywq.csb.app/room/" +
 				roomID +
 				`?avatarNum=${chatAvatar}&avatarColor=${chatColor}&chatBg=${chatBg}`
 		);
